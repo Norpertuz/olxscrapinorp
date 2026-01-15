da0ecb7489979eaccc97f5e1eaf41fd08e8f5a33
Norpertuz <norpertuz@gmail.com>
2026-01-15 23:30:09 +0100

    Wycinka2

--- server.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 8000
-
-def run():
-    with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
-        print(f"http://localhost:{PORT}")
-        httpd.serve_forever()
