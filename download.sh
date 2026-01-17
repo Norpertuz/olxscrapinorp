93e69440616bf9f9d4fe9002e0cce004df63e63f
Norpertuz <norpertuz@gmail.com>
2026-01-17 13:07:27 +0100

    fixed download count

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Komputery stacjonarne ---
-for page in {1..2}; do
+for page in {1..25}; do
     file="files/olx_page_pc_${page}.html"
     echo "Pobieram stronę PC $page → $file"
 
@@ -11,7 +11,7 @@ for page in {1..2}; do
 done
 
 # --- Karty graficzne ---
-for page in {1..2}; do
+for page in {1..25}; do
     file="files/olx_page_gpu_${page}.html"
     echo "Pobieram stronę GPU $page → $file"
 
@@ -20,7 +20,7 @@ for page in {1..2}; do
     "https://www.olx.pl/elektronika/komputery/podzespoly-i-czesci/karty-graficzne/?search%5Bfilter_float_price:from%5D=10&search%5Bfilter_float_price:to%5D=150&view=grid&page=$page"
 done
 
-for page in {1..2}; do
+for page in {1..25}; do
     file="files/olx_page_laptop_${page}.html"
     echo "Pobieram stronę Laptop $page → $file"
 
@@ -29,7 +29,7 @@ for page in {1..2}; do
     "https://www.olx.pl/elektronika/komputery/laptopy/?search%5Bfilter_float_price:from%5D=50&search%5Bfilter_float_price:to%5D=650&view=grid&page=$page"
 done
 
-for page in {1..2}; do
+for page in {1..25}; do
     file="files/olx_page_cpu_${page}.html"
     echo "Pobieram stronę CPU $page → $file"
 
@@ -38,7 +38,7 @@ for page in {1..2}; do
     "https://www.olx.pl/elektronika/komputery/podzespoly-i-czesci/procesory/?search%5Bfilter_float_price:from%5D=10&search%5Bfilter_float_price:to%5D=300&view=grid&page=$page"
 done
 
-for page in {1..2}; do
+for page in {1..25}; do
     file="files/olx_page_ram_${page}.html"
     echo "Pobieram stronę RAM $page → $file"
 
